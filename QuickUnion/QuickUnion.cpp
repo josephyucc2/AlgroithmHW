573edd941a5f14333e1c7ea54a87b36a2e16a89d
josephyucc2git config --global user.email josephyucc2@outlook.com## Ignore Visual Studio temporary files, build results, and## files generated by popular Visual Studio add-ons.#### Get latest from https://github.com/github/gitignore/blob/master/VisualStudio.gitignore# User-specific files*.suo*.user*.userosscache*.sln.docstates# User-specific files (MonoDevelop/Xamarin Studio)*.userprefs# Build results[Dd]ebug/[Dd]ebugPublic/[Rr]elease/[Rr]eleases/x64/x86/bld/[Bb]in/[Oo]bj/[Ll]og/# Visual Studio 2015/2017 cache/options directory.vs/# Uncomment if you have tasks that create the project's static files in wwwroot#wwwroot/# Visual Studio 2017 auto generated filesGenerated\ Files/# MSTest test Results[Tt]est[Rr]esult*/[Bb]uild[Ll]og.*# NUNIT*.VisualState.xmlTestResult.xml# Build Results of an ATL Project[Dd]ebugPS/[Rr]eleasePS/dlldata.c# Benchmark ResultsBenchmarkDotNet.Artifacts/# .NET Coreproject.lock.jsonproject.fragment.lock.jsonartifacts/**/Properties/launchSettings.json# StyleCopStyleCopReport.xml# Files built by Visual Studio*_i.c*_p.c*_i.h*.ilk*.meta*.obj*.pch*.pdb*.pgc*.pgd*.rsp*.sbr*.tlb*.tli*.tlh*.tmp*.tmp_proj*.log*.vspscc*.vssscc.builds*.pidb*.svclog*.scc# Chutzpah Test files_Chutzpah*# Visual C++ cache filesipch/*.aps*.ncb*.opendb*.opensdf*.sdf*.cachefile*.VC.db*.VC.VC.opendb# Visual Studio profiler*.psess*.vsp*.vspx*.sap# Visual Studio Trace Files*.e2e# TFS 2012 Local Workspace/# Guidance Automation Toolkit*.gpState# ReSharper is a .NET coding add-in_ReSharper*/*.[Rr]e[Ss]harper*.DotSettings.user# JustCode is a .NET coding add-in.JustCode# TeamCity is a build add-in_TeamCity*# DotCover is a Code Coverage Tool*.dotCover# AxoCover is a Code Coverage Tool.axoCover/*# Visual Studio code coverage results*.coverage*.coveragexml# NCrunch_NCrunch_*.*crunch*.local.xmlnCrunchTemp_*# MightyMoose*.mm.*AutoTest.Net/# Web workbench (sass).sass-cache/# Installshield output folder[Ee]xpress/# DocProject is a documentation generator add-inDocProject/buildhelp/DocProject/Help/*.HxTDocProject/Help/*.HxCDocProject/Help/*.hhcDocProject/Help/*.hhkDocProject/Help/*.hhpDocProject/Help/Html2DocProject/Help/html# Click-Once directorypublish/# Publish Web Output*.[Pp]ublish.xml*.azurePubxml# Note: Comment the next line if you want to checkin your web deploy settings,# but database connection strings (with potential passwords) will be unencrypted*.pubxml*.publishproj# Microsoft Azure Web App publish settings. Comment the next line if you want to# checkin your Azure Web App publish settings, but sensitive information contained# in these scripts will be unencryptedPublishScripts/# NuGet Packages*.nupkg# The packages folder can be ignored because of Package Restore**/[Pp]ackages/*# except build/, which is used as an MSBuild target.config --global user.name josephyucc2 <josephyucc2@outlook.com>
2018-03-15 11:25:40 +0800

    Add project files.

---
+++ QuickUnion/QuickUnion.cpp
@@ -0,0 +1,129 @@
+// hw1.cpp : Defines the entry point for the console application.
+//
+
+#include "iostream"
+#include"fstream"
+
+using namespace std;
+
+class quickUnion
+{
+public:
+	int*id;
+	quickUnion();
+	quickUnion(int n);
+	int root(int i);
+	bool connected(int p, int q);
+	void Union(int p, int q);
+};
+void output(quickUnion node);
+quickUnion::quickUnion()
+{
+	id = new int[0];
+}
+quickUnion::quickUnion(int n)
+{
+	id = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		id[i] = i;
+	}
+}
+int quickUnion::root(int i)
+{
+	while (i != id[i])
+	{
+		i = id[i];
+	}
+	return i;
+}
+bool quickUnion::connected(int p, int q)
+{
+	return (root(p) == root(q));
+}
+void quickUnion::Union(int p, int q)
+{
+	if (connected(p, q))
+	{
+		cout << "Connect." << endl;
+		return;
+	}
+	else
+	{
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+	}
+}
+void output(quickUnion node)
+{
+
+}
+int main()
+{
+
+	int dataNum;
+	int runtime;
+	while (cin >> dataNum)
+	{
+		quickUnion node(dataNum);
+		cout << "value  ";
+		for (int i = 0; i < dataNum; i++)
+		{
+
+			if (i == dataNum - 1)
+			{
+				cout << i << endl;
+			}
+			else
+				cout << i << " ";
+		}
+		cout << "father  ";
+		for (int i = 0; i < dataNum; i++)
+		{
+
+			if (i == dataNum - 1)
+			{
+				cout << node.id[i];
+				cout << endl;
+			}
+			else
+				cout << node.id[i] << " ";
+		}
+		cin >> runtime;
+
+		for (int i = 0; i < runtime; i++)
+		{
+			int p, q;
+			cin >> p >> q;
+			node.Union(p, q);
+			cout << "value  ";
+			for (int i = 0; i < dataNum; i++)
+			{
+
+				if (i == dataNum - 1)
+				{
+					cout << i << endl;
+				}
+				else
+					cout << i << " ";
+			}
+			cout << "father  ";
+			for (int i = 0; i < dataNum; i++)
+			{
+
+				if (i == dataNum - 1)
+				{
+					cout << node.id[i];
+					cout << endl;
+				}
+				else
+					cout << node.id[i] << " ";
+			}
+		}
+	}
+
+	return 0;
+}
+
